948f89d24bb1d2d8e037963e3bd3aaa9ea2837e6
BabyMagikarp <adrifelcha@gmail.com>
2023-11-30 13:15:22 -0800

    Customary function: Write .bug files containing JAGS models

---
+++ code/functions/jags_writeModel.R
@@ -0,0 +1,59 @@
+#####################################################################
+# Functions to automatically write desired JAGS models
+#####################################################################
+
+# A function to write the JAGS model using the prior values
+write_JAGSmodel <- function(prior, modelFile = "EZHBDDM.bug"){
+  opening <- "model{"
+  prior.bound_m <- paste("          bound_mean ~ dnorm(", prior$bound_mean_mean,",pow(",prior$bound_mean_sdev,",-2))T(0.10,3.00)", sep="")
+  prior.nondt_m <- paste("          nondt_mean ~ dnorm(", prior$nondt_mean_mean,",pow(",prior$nondt_mean_sdev,",-2))T(0.05,1.00)", sep="")
+  prior.bound_sd <- paste("          bound_sdev ~ dunif(", prior$bound_sdev_lower,",",prior$bound_sdev_upper,")", sep="")
+  prior.nondt_sd <- paste("          nondt_sdev ~ dunif(", prior$nondt_sdev_lower,",",prior$nondt_sdev_upper,")", sep="")
+  prior.drift_sd <- paste("          drift_sdev ~ dunif(", prior$drift_sdev_lower,",",prior$drift_sdev_upper,")", sep="")
+  priors <- c(prior.bound_m, prior.nondt_m, prior.bound_sd, prior.nondt_sd, prior.drift_sd)
+  if(!is.null(prior$drift_mean_mean)){
+    prior.drift_m <- paste("          drift_mean ~ dnorm(", prior$drift_mean_mean,",pow(",prior$drift_mean_sdev,",-2))T(-3.00,3.00)", sep="")
+    priors <- c(priors, prior.drift_m)
+  }
+  if(!is.null(prior$drift_intercept_mean)){
+    prior.drift_i <- paste("          drift_intrcpt ~ dnorm(", prior$drift_intercept_mean,",pow(",prior$drift_intercept_sdev,",-2))T(-3.00,3.00)", sep="")
+    priors <- c(priors, prior.drift_i)
+  }
+  if(!is.null(prior$drift_coefficient_mean)){
+    prior.drift_c <- paste("          drift_coeff ~ dnorm(", prior$drift_coefficient_mean,",pow(",prior$drift_coefficient_sdev,",-2))T(-3.00,3.00)", sep="")
+    priors <- c(priors, prior.drift_c)
+  }
+  
+  content.init <-"
+              # Sampling model
+              for (p in 1:nParticipants){
+                  bound[p] ~ dnorm(bound_mean, pow(bound_sdev, -2))T(0.10,3.00)
+                  nondt[p] ~ dnorm(nondt_mean, pow(nondt_sdev, -2))T(0.05,1.00)"
+  if(!is.null(prior$drift_intercept_mean)){
+    content.mid <- "                  drift_mean = drift_intrcpt"
+    if(!is.null(prior$drift_coefficient_mean)){
+      content.mid <- paste(content.mid," + (drift_coeff * x[p])", sep="") 
+    }
+    content.init <- c(content.init,content.mid)
+  }
+  content.end <- "                  drift[p] ~ dnorm(drift_mean, pow(drift_sdev, -2))T(-3.00,3.00)
+      
+                  # Forward equations from EZ Diffusion
+                  ey[p]  = exp(-bound[p] * drift[p])
+                  Pc[p]  = 1 / (1 + ey[p])
+                  PRT[p] = 2 * pow(drift[p], 3) / bound[p] * pow(ey[p] + 1, 2) / (2 * -bound[p] * drift[p] * ey[p] - ey[p]*ey[p] + 1)
+                  MDT[p] = (bound[p] / (2 * drift[p])) * (1 - ey[p]) / (1 + ey[p])
+                  MRT[p] = MDT[p] + nondt[p]
+
+                  # Loss functions using MRT, PRT, and Pc
+                  meanRT[p]  ~ dnorm(MRT[p], PRT[p] * correct[p])
+                  varRT[p]   ~ dnorm(1/PRT[p], 0.5 * correct[p] * PRT[p] * PRT[p])
+                  correct[p] ~ dbin(Pc[p], nTrialsPerPerson)
+              }
+      }"
+  content <- c(content.init, content.end)
+  
+  final_file <- file(modelFile)
+  writeLines(c(opening,priors,content), final_file)
+  close(final_file)
+}
