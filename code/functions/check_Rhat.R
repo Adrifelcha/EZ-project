c99ebf1132dca118c383f30dbe62c1cd872566cb
Adrifelcha <adrifelcha@gmail.com>
2024-06-08 02:07:56 -0700

    Correct: check_Rhat() function, which was imported from a past version of the project

--- code/functions/check_Rhat.R
+++ code/functions/check_Rhat.R
@@ -1,21 +1,18 @@
 # A function to check the Rhats for a simulation
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~!
-check_Rhat <- function(simOutput){
+check_Rhat <- function(rhats){
   rule <- 1.05
-  rhats <-simOutput$sim_rhats
-  bad.Rhat <- which(rhats>rule,arr.ind = TRUE)
+  bad.Rhat <- which(as.numeric(rhats)>rule)
   test.rhat <- length(bad.Rhat) > 0
-  if(!test.rhat){
+  if(test.rhat){
     par(mfrow=c(1,1))
-    bad.Rhat.ID <- colnames(rhats)[bad.Rhat[,2]]
-    
-    hist(rhats[which(rhats>rule)],
-         main=paste("Rhats > or = than ", rule))
+    which.are.bad.Rhats <- names(rhats)[bad.Rhat]
+    hist(rhats)
     abline(v=rule, col="red", lty=2)
-    legend("top",paste("Rhat = ",rule," | ",
-                       (round(nrow(bad.Rhat)/prod(dim(rhats)),5))*100,
+    legend("top",paste("Rhat > ",rule," | ",
+                       (round(length(bad.Rhat)/(length(rhats)),5))*100,
                        "% of chains | ", length(bad.Rhat.ID), " chains", sep=""), lty=2, col="red", cex=0.4)
-    table(bad.Rhat.ID)
+    table(which.are.bad.Rhats)
   }else{
     paste("No Rhat greater than ", rule, sep="")      
   }
