d6e0c55a4e59d3b99c85a0736f0bae61bc28110e
Adrifelcha <adrifelcha@gmail.com>
2024-06-07 21:27:45 -0700

    Correct: We use ALL RTs

--- code/functions/getStatistics.R
+++ code/functions/getStatistics.R
@@ -1,5 +1,5 @@
 # Get individual statistics from raw data: mean accuracy and mean and variance of correct-RT
-get_Statistics <- function(data){
+getStatistics <- function(data){
   if(is.null(data$accuracy)|is.null(data$rt)){
     error.msg = "Data not available."
     return(print(error.msg))
@@ -15,14 +15,11 @@ get_Statistics <- function(data){
   }
   # Get proportion of correct responses
   mean_accuracy <- tapply(data$accuracy, data$sub, mean) 
-  # Get mean and variance of correct RT
-  keep.correct <- which(data$accuracy==1)
-  correct_only <- data[keep.correct,]
-  mean_rt_correct <- tapply(correct_only$rt, correct_only$sub, mean)
-  var_rt_correct  <- tapply(correct_only$rt, correct_only$sub, var)
+  mean_rt <- tapply(data$rt, data$sub, mean)
+  var_rt  <- tapply(data$rt, data$sub, var)
   # Create a data.frame with just summary statistics
-  data_statistics <- cbind(subID, sum_correct, mean_accuracy, mean_rt_correct, var_rt_correct)
+  data_statistics <- cbind(subID, sum_correct, mean_accuracy, mean_rt, var_rt)
   data_statistics <- as.data.frame(data_statistics)
-  colnames(data_statistics) = c("sub", "sum_correct","meanAccuracy", "meanRT_correct", "varRT_correct")
+  colnames(data_statistics) = c("sub", "sum_correct","meanAccuracy", "meanRT", "varRT")
   return(data_statistics)
 }
