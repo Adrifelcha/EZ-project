b412d27566389b71fe76400511f037839bc4b833
Adrifelcha <adrifelcha@gmail.com>
2024-06-10 17:21:53 -0700

    Fix: This function will now be used after running all Datasets, so it needs to work with matrices and not a vector

--- code/functions/check_Rhat.R
+++ code/functions/check_Rhat.R
@@ -2,18 +2,16 @@
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~!
 check_Rhat <- function(rhats){
   rule <- 1.05
-  bad.Rhat <- which(as.numeric(rhats)>rule)
-  test.rhat <- length(bad.Rhat) > 0
+  bad.Rhat <- which(rhats>rule, arr.ind = TRUE)
+  test.rhat <- nrow(bad.Rhat) > 0
   if(test.rhat){
-    par(mfrow=c(1,1))
-    which.are.bad.Rhats <- names(rhats)[bad.Rhat]
-    hist(rhats)
-    abline(v=rule, col="red", lty=2)
-    legend("top",paste("Rhat > ",rule," | ",
-                       (round(length(bad.Rhat)/(length(rhats)),5))*100,
-                       "% of chains | ", length(bad.Rhat.ID), " chains", sep=""), lty=2, col="red", cex=0.4)
-    table(which.are.bad.Rhats)
-  }else{
-    paste("No Rhat greater than ", rule, sep="")      
-  }
+          par(mfrow=c(1,1))
+          which.are.bad.Rhats <- colnames(rhats)[bad.Rhat[,2]]
+          hist(rhats, breaks = 50)
+          abline(v=rule, col="red", lty=2)
+          legend("top",paste("Rhat > ",rule," | ",
+                             (round(length(bad.Rhat)/(length(rhats)),5))*100,
+                             "% of chains | ", length(which.are.bad.Rhats), " chains", sep=""), lty=2, col="red", cex=0.4)
+          table(which.are.bad.Rhats)
+  }else{      paste("No Rhat greater than ", rule, sep="")       }
 }
