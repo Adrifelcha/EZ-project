ec45a8dd268ebd0b248201ab3e5d75579503097e
Adriana Chavez <adrifelcha@gmail.com>
2024-06-11 14:32:04 -0700

    Add: modelType argument to distinguish between hierarchical-metaregression-ttest simulation studies

--- code/functions/namingFunctions.R
+++ code/functions/namingFunctions.R
@@ -1,5 +1,13 @@
-nameOutput <- function(nTrials, nParticipants, nDatasets,Design.Label="EZBHDDM"){
-  fileName <- paste("./sim_P",nParticipants,"T",nTrials,"D",nDatasets,"_",
-                    Design.Label,".RData", sep="")
+nameOutput <- function(nTrials, nParticipants, nDatasets, modelType=NA){
+  if(is.na(modelType)|modelType=="hierarchical"){
+          fileName <- paste("./sim_P",nParticipants,"T",nTrials,"D",nDatasets,
+                            "_EZBHDDM.RData", sep="")
+  }else{if(modelType=="metaregression"){
+          fileName <- paste("./sim_P",nParticipants,"T",nTrials,"D",nDatasets,
+                            "_MetaRegEZBHDDM.RData", sep="")
+  }else{if(modelType=="ttest"){
+          fileName <- paste("./sim_P",nParticipants,"T",nTrials,"D",nDatasets,
+                            "_ttestEZBHDDM.RData", sep="")
+  }else{  stop("Invalid modelType")  }}}
   return(fileName)
 }
