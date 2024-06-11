0c282d3732eb3ff5f49c63d22a4110fb53a80362
Adriana Chavez <adrifelcha@gmail.com>
2024-06-11 15:42:00 -0700

    Add: When working with modelType='metaregression', the default priors include a betaweight

--- code/functions/default_priors.R
+++ code/functions/default_priors.R
@@ -1,10 +1,14 @@
-default_priors <- function(Show=TRUE){
+default_priors <- function(Show=TRUE, modelType=NA){
   prior <- data.frame("bound_mean_mean" = 1.50,  "bound_mean_sdev" = 0.20,
                       "drift_mean_mean" = 0.00,  "drift_mean_sdev" = 0.50,
                       "nondt_mean_mean" = 0.30,  "nondt_mean_sdev" = 0.06,
-                      "bound_sdev_lower" = 0.10, "bound_sdev_upper" = 0.20,
+                      "bound_sdev_lower" = 0.10, "bound_sdev_upper" = 0.40,
                       "drift_sdev_lower" = 0.20, "drift_sdev_upper" = 0.40,
-                      "nondt_sdev_lower" = 0.05, "nondt_sdev_upper" = 0.1)
+                      "nondt_sdev_lower" = 0.05, "nondt_sdev_upper" = 0.25)
+  if((!is.na(modelType))&modelType=="metaregression"){
+            prior$betaweight_lower = 0.00
+            prior$betaweight_upper = 1.00
+  }
   if(Show){       show_priors(prior)           }
   return(prior)
 }
