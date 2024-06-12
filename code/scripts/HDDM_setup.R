6d4a6d1b505acb1b3456662bd68aefcb575737d2
Adrifelcha <adrifelcha@gmail.com>
2024-06-11 23:57:02 -0700

    Major fix: The general setup (i.e. creation of the 'settings' main object and the list of priors) is moved to the main HDDM_runSims() function. Additionally, we implement changes since we have defined that there are no replicate datasets with the same set of parameter values

--- code/scripts/HDDM_setup.R
+++ code/scripts/HDDM_setup.R
@@ -1,40 +1,19 @@
-HDDM_setup <-function(nParticipants, nTrials, nDatasets=1, modelType=NA, criterion=NA, priors=NA, Show=TRUE){
-    ############################################################################
-    # Part 1: Parameter set up
-    ############################################################################
-    # Set up PRIORS
-    if(sum(is.na(priors))>0){    priors <- default_priors(Show, modelType)     }
-    # Identify and (optionally) print the design settings to screen
-    settings <- list("nPart"= nParticipants, "nTrials"= nTrials, "prior"= priors, "criterion" = criterion)
-    if(!(modelType=="hierarchical"|is.na(modelType))){   
-        X <- 0:(settings$nPart-1)                      # Default predictor       
-        if(modelType=="ttest"){   X <- X %% 2    }     # Dummy predictor
-        settings <- c(settings, "X" = X)
-    }
-    if(Show){  show_design(settings)  }
+HDDM_setup <-function(settings, modelType=NA, criterion=NA, priors=NA, Show=TRUE){
+    nParticipants <- settings$nPart
+    nTrials <- settings$nTrials
+  
     # Sample "true parameters" for the simulation using the priors
-    parameter_set <- sample_parameters(settings, modelType, Show)
+    parameter_set <- sample_parameters(settings, modelType, Show)  
     
-    ############################################################################
-    # Part 2: Prepare data
-    ############################################################################
-    # Generate a single (first) data set using the settings established above
     rawData = sample_data(settings,parameter_set)
     summData = getStatistics(rawData) 
-    # Establish two arrays to store nDatasets, with the first page already filled
-    rawData <- array(c(rawData, rep(NA,prod(nParticipants*nTrials,3,nDatasets-1))), 
-                     dim=c(nParticipants*nTrials,3,nDatasets), 
+    # Establish two arrays to store the data, with the first page already filled
+    rawData <- array(c(rawData, rep(NA,prod(nParticipants*nTrials,3,1))), 
+                     dim=c(nParticipants*nTrials,3,1), 
                      dimnames = list(NULL, colnames(rawData), NULL))
-    summData <- array(c(summData, rep(NA,prod(nParticipants*nTrials,5,nDatasets-1))),
-                      dim=c(nParticipants,5,nDatasets),
+    summData <- array(c(summData, rep(NA,prod(nParticipants*nTrials,5,1))),
+                      dim=c(nParticipants,5,1),
                       dimnames = list(NULL, colnames(summData), NULL))
-    if(nDatasets>1){
-          for(i in 2:nDatasets){
-              rawData[,,i] = sample_data(settings,parameter_set)
-              summData[,,i] = as.matrix(getStatistics(rawData[,,i]))
-              cat("Generating dataset", i, "of", nDatasets,"\n")
-          }
-    }
     return(list("settings" = settings, "parameter_set" = parameter_set, "priors" = priors,
                 "rawData" = rawData, "sumData" = summData))
 }
