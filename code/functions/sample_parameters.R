5d010f533eb7c0aff0d37328de9cf0e295077eaf
Adriana Chavez <adrifelcha@gmail.com>
2024-06-11 19:06:47 -0700

    Add: Function distinguishes cases where we need a predictor variable and a coefficient, then we generate individual parameters for the criterion-parameter using said coefficient

--- code/functions/sample_parameters.R
+++ code/functions/sample_parameters.R
@@ -1,13 +1,9 @@
 # A function to sample true parameter values from the priors specified
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~!
-sample_parameters <- function(settings, Show=TRUE){
+sample_parameters <- function(settings, modelType, Show=TRUE){
   prior <- settings$prior
   bound_mean <- rnorm(1,prior$bound_mean_mean,prior$bound_mean_sdev)
-  if(is.null(prior$drift_intercept_mean)){
-    drift_mean <- rnorm(1,prior$drift_mean_mean,prior$drift_mean_sdev)
-  }else{
-    drift_mean <- 0
-  }
+  drift_mean <- rnorm(1,prior$drift_mean_mean,prior$drift_mean_sdev)
   nondt_mean <- rnorm(1,prior$nondt_mean_mean,prior$nondt_mean_sdev)
   bound_sdev <- runif(1,prior$bound_sdev_lower,prior$bound_sdev_upper)  
   drift_sdev <- runif(1,prior$drift_sdev_lower,prior$drift_sdev_upper)
@@ -18,6 +14,21 @@ sample_parameters <- function(settings, Show=TRUE){
   parameter_set <- list("bound_mean" = bound_mean, "drift_mean" = drift_mean, "nondt_mean" = nondt_mean, 
                         "bound_sdev" = bound_sdev, "drift_sdev" = drift_sdev, "nondt_sdev" = nondt_sdev,
                         "bound" = bound,   "drift" = drift,   "nondt" = nondt)
-  if(Show){         show_parameters(parameter_set)              }
+  # Check modelType to determine the need for a predictor and coefficient
+  if(!(modelType=="hierarchical"|is.na(modelType))){   
+           X <- 0:(settings$nPart-1)                      # Default predictor       
+           if(modelType=="ttest"){   X <- X %% 2    }     # Dummy predictor
+           # Sample and add coefficient to the parameter_set
+           betaweight <- runif(1, prior$betaweight_lower, prior$betaweight_upper)
+           parameter_set <- c(parameter_set, list("betaweight" = betaweight, "X" = X))
+           # Identify criterion (i.e., parameter of interest)
+           if(is.na(settings$criterion)){  settings$criterion <- "drift"  }
+           crit <- settings$criterion
+           # Use coefficient to generate individual true parameters for the criterion
+           if(crit=="bound"){  parameter_set$bound <- rnorm(settings$nPart,bound_mean+betaweight*X, bound_sdev)  }
+           if(crit=="drift"){  parameter_set$drift <- rnorm(settings$nPart,drift_mean+betaweight*X, drift_sdev)  }
+           if(crit=="nondt"){  parameter_set$nondt <- rnorm(settings$nPart,nondt_mean+betaweight*X, nondt_sdev)  }
+     }
+  if(Show){   show_parameters(parameter_set)    }
   return(parameter_set)
 }
