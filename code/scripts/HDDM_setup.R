f89e881f6edb08bdc6184e3361a42a09606cf93f
Adrifelcha <adrifelcha@gmail.com>
2024-06-12 00:59:35 -0700

    Fix: Since we will only generate one Dataset, we don't really need to create any storing array, we can simply pass the output directly

--- code/scripts/HDDM_setup.R
+++ code/scripts/HDDM_setup.R
@@ -1,19 +1,10 @@
-HDDM_setup <-function(settings, modelType=NA, criterion=NA, priors=NA, Show=TRUE){
+HDDM_setup <-function(settings, modelType=NA, Show=TRUE){
     nParticipants <- settings$nPart
     nTrials <- settings$nTrials
-  
     # Sample "true parameters" for the simulation using the priors
     parameter_set <- sample_parameters(settings, modelType, Show)  
-    
+    # Generate data
     rawData = sample_data(settings,parameter_set)
     summData = getStatistics(rawData) 
-    # Establish two arrays to store the data, with the first page already filled
-    rawData <- array(c(rawData, rep(NA,prod(nParticipants*nTrials,3,1))), 
-                     dim=c(nParticipants*nTrials,3,1), 
-                     dimnames = list(NULL, colnames(rawData), NULL))
-    summData <- array(c(summData, rep(NA,prod(nParticipants*nTrials,5,1))),
-                      dim=c(nParticipants,5,1),
-                      dimnames = list(NULL, colnames(summData), NULL))
-    return(list("settings" = settings, "parameter_set" = parameter_set, "priors" = priors,
-                "rawData" = rawData, "sumData" = summData))
+    return(list("parameter_set" = parameter_set, "rawData" = rawData, "sumData" = summData))
 }
