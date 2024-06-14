3cb62a3b15a7195b132eb74f74517198fd3a97ce
Adrifelcha <adrifelcha@gmail.com>
2024-06-13 17:47:08 -0700

    Small fix: To avoid problems with init values, we reduce the standard deviation of the init values sampled for the drift rate

--- code/functions/default_inits.R
+++ code/functions/default_inits.R
@@ -1,7 +1,7 @@
 default_inits <- function(n.chains,nParticipants){
   myinits <- rep(list(list()), n.chains)
   for(i in 1:n.chains){
-    myinits[[i]] <- list(drift = rnorm(nParticipants,0,0.1))
+    myinits[[i]] <- list(drift = rnorm(nParticipants,0,0.05))
   }
   return(myinits)
 }
