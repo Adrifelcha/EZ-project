ab0d276a7172d9b3b7b206f34480fcbeb3231923
Adriana Chavez <adrifelcha@gmail.com>
2024-06-14 21:37:52 -0700

    Add: Arranging the output from this function by separating the output generated with the simple hierarchical modelType and the metaregression&ttest modelType, which include an additional parameter and covariate

--- code/scripts/HDDM_runFullSeed.R
+++ code/scripts/HDDM_runFullSeed.R
@@ -1,39 +1,52 @@
 HDDM_runFullSeed <- function(seed, settings){
+  set.seed(seed)
   suppressMessages(library(R2jags))
   suppressMessages(library(rstan))
   
-  out <- list()
-  for(p in settings$participant_levels){
-      X <- cbind(rep(NA,p),(0:(p-1))/p,(0:(p-1))%%2)
-      colnames(X) <- settings$design_levels
-    
-      for(d in settings$design_levels){
+  ########## Print a file to indicate the start of a new seed ###############
+  write('Seed has been initiated', paste(settings$output.folder,"seed-",seed,"_start.txt",sep=""))
+  
+  out_H <- list()
+  out_Beta <- list()
+  cell <- 0
+  for(d in settings$design_levels){
+      
+      for(p in settings$participant_levels){
+          X <- cbind(rep(NA,p),(0:(p-1))%%2,(0:(p-1))/p)
+          colnames(X) <- settings$design_levels
           
           for(t in settings$trial_levels){
             
               if(d=="hierarchical"){
                 design <- HDDM_setup(settings$priors[[d]], p, t, d, X[,d], criterion = NA, settings$fromPrior, Show = FALSE)
-                tic <- clock::date_now(zone="UTC")
                 runJags <- HDDM_runJAGS(summaryData = design$sumData, t, X[,d], settings$jagsData[[d]], 
-                                        settings$jagsParameters[[d]], settings$jagsInits,  settings$n.chains,
-                                        settings$modelFile[d,1], Show = FALSE)
-                toc <- clock::date_now(zone="UTC")
-                clock <- as.numeric(toc-tic, units="secs")  # Record time
+                                        settings$jagsParameters[[d]], settings$jagsInits[[as.character(p)]],  
+                                        settings$n.chains, settings$modelFile[d,1], Show = FALSE)
+                out_H <- rbind(out_H,list(seed = seed, p = p, t = t, d = d, rhats = runJags$rhats,
+                               true.values    = design$parameter_set, mean.estimates = runJags$estimates,
+                               std.estimates  = runJags$estimates, elapsed.time   = runJags$clock))
+                cell <- 1 + cell
+                cat("Iteration", cell, "of",  settings$nCells,"\n")
               }else{
-                    for(c in criterion_levels){
+                    for(c in settings$criterion_levels){
                         design <- HDDM_setup(settings$priors[[d]], p, t, d, X[,d], c, settings$fromPrior, Show = FALSE)
-                        tic <- clock::date_now(zone="UTC")
                         runJags <- HDDM_runJAGS(summaryData = design$sumData, t, X[,d], settings$jagsData[[d]], 
-                                                settings$jagsParameters[[d]], settings$jagsInits,  settings$n.chains, 
-                                                settings$modelFile[d,c], Show = FALSE)
-                        toc <- clock::date_now(zone="UTC")
-                        clock <- as.numeric(toc-tic, units="secs")  # Record time
+                                                settings$jagsParameters[[d]], settings$jagsInits[[as.character(p)]],
+                                                settings$n.chains, settings$modelFile[d,c], Show = FALSE)
+                        out_Beta <- rbind(out_Beta,list(seed = seed, p = p, t = t, d = d, c = c, rhats = runJags$rhats,
+                                          true.values    = design$parameter_set, mean.estimates = runJags$estimates,
+                                          std.estimates  = runJags$estimates, elapsed.time   = runJags$clock))
+                        cell <- 1 + cell
+                        cat("Iteration", cell, "of",  settings$nCells,"\n")
                     }
               }
           }
       }
   }
-    save(output, file="seed1.RData")  
+  ########## Print a file to indicate the end of a the seed ###############
+  write('Seed has ended running', paste(settings$output.folder,"seed-",seed,"_end.txt",sep=""))
+  save(output, file=paste(settings$output.folder,"seed-",seed,".RData",sep=""))  
+  return(output)
 }
   
   
