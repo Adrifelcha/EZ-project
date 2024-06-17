f01ccb42dbbd6b0fcfbe3ac5eac17541b92c692b
Adrifelcha <adrifelcha@gmail.com>
2024-06-17 12:51:35 -0700

    Start: A new function specifically designed to run the simple t-test comparison effect

---
+++ code/scripts/HDDM_simFixedEffect.R
@@ -0,0 +1,124 @@
+HDDM_runSims <- function(nParticipants, nTrialsPerCondition, nDatasets = 10, beta.effect = 0, 
+                         priors = NA, n.chains = 4,  Show=TRUE, forceSim = FALSE, fromPrior=TRUE){
+    #################################
+    # Initial checks
+    #################################
+    # Load necessary R libraries
+    suppressMessages(library(R2jags))
+    suppressMessages(library(rstan))
+    # Identify output File
+    outputFile <- paste("./sim_P",nParticipants,"T",nTrials,"D",nDatasets,
+                        "_FixedEffect.RData", sep="")
+    
+    # Check if we needToRun simulations again (overruled by 'forceSim')
+    if(!forceSim){
+          if(file.exists(outputFile)){
+              if(sum(is.na(priors))>0){  myPriors <- default_priors(Show = FALSE, modelType = "ttest")
+                                 }else{  myPriors <- priors}
+              myNChains <- n.chains
+              load(outputFile)                    
+              checkPriors <- sum(output$settings$prior != myPriors, na.rm = TRUE)
+              checkNChains <- sum(output$n.chains != myNChains, na.rm = TRUE)
+              needToRun <- sum(checkPriors,checkNChains)>0
+          }else{  needToRun <- TRUE  }
+    }else{  needToRun <- TRUE  }
+    
+    ###################################
+    # Run simulation study (if needed)
+    ###################################
+    if(needToRun){
+            ######################
+            #      SET UP        #
+            ######################
+            # ~~~~~~~~~~~~~~~~ Settings
+            # Define the design settings according to modelType and (optionally) print to screen
+            settings <- list("nPart"= nParticipants, "nTrialsPerCondition"= nTrialsPerCondition,
+                             "effect" = beta.effect, "nDatasets" = nDatasets)
+            if(Show){  show_design(settings)  }
+            # If the model includes an effect (betaweight)
+            X <- rep(c(1,0),nParticipants)
+            # Load default priors if needed and add to settings
+            if(sum(is.na(priors))>0){    priors <- default_priors(Show, modelType="ttest")    }else{
+                            if(Show){    show_priors(priors)}                         }
+            settings <- c(settings, list("prior" = priors))
+            # ~~~~~~~~~~~~~~~~ JAGS variables
+            # Define parameters to be tracked on JAGS, according to the modelType
+            jagsParameters <- c("bound_mean", "drift_mean", "drift_cond", "nondt_mean", "bound", "nondt",
+                                "drift_sdev", "nondt_sdev", "bound_sdev", "drift", "betaweight")
+            # Write pertinent JAGS model
+            modelFile <- "./FixedEffect.bug"  
+            writefixEffJAGSmodel(priors, beta.effect)
+            # Data to be passed to JAGS
+            jagsData = data_toJAGS(modelType="ttest")
+            # init values
+            jagsInits <- default_inits(n.chains, nParticipants*2)  
+            # ~~~~~~~~~~~~~~~~ Storing objects
+            # Count number of parameters (i.e. we always assume individual parameters)
+            nParams <- (length(jagsParameters)-3) + (nParticipants*3)
+            MatEstimates <- matrix(NA, nrow=nDatasets, ncol=nParams)
+            MatTrueVal   <- matrix(NA, nrow=nDatasets, ncol=nParams)
+            ArrayCredInt <- array(NA, dim=c(nDatasets,nParams,2))
+            MatRhats     <- matrix(NA, nrow=nDatasets, ncol=(nParams+1))
+            # ~~~~~~~~~~~~~~~~~~ Only Show output for a random iteration
+            showChains <- rep(FALSE,nDatasets)
+            if(Show){showChains[sample(nDatasets,1)] <- TRUE}
+            ######################
+            #   Run iterations   #
+            ######################
+            for(k in 1:nDatasets){
+                set.seed(k)
+                cat("============>> Dataset", k, "of", nDatasets,"\n")
+                design <- HDDM_setup(priors, nParticipants, nTrials, modelType, X, criterion, fromPrior, Show=FALSE)
+                runJags <- HDDM_runJAGS(summaryData = design$sumData, nTrials, X, 
+                                        jagsData, jagsParameters, jagsInits, 
+                                        n.chains, modelFile, Show = showChains[k])  
+                MatRhats[k,] <- runJags$rhats
+                c <- 0; d <- 0
+                for(j in 1:length(runJags$estimates)){
+                   m <- length(runJags$estimates[[j]])
+                   w <- length(design$parameter_set[[j]])
+                   MatEstimates[k,(c+1):(c+m)] <- runJags$estimates[[j]]
+                   MatTrueVal[k,(d+1):(d+w)]   <- design$parameter_set[[j]]
+                   if(is.vector(runJags$credInterval[[j]])){
+                         ArrayCredInt[k,(c+1):(c+m),1] <- runJags$credInterval[[j]][1]
+                         ArrayCredInt[k,(c+1):(c+m),2] <- runJags$credInterval[[j]][2]
+                   }else{
+                         ArrayCredInt[k,(c+1):(c+m),1] <- runJags$credInterval[[j]][1,]
+                         ArrayCredInt[k,(c+1):(c+m),2] <- runJags$credInterval[[j]][2,]
+                   }
+                   c <- c+m; d <- d+w
+                }
+            }
+            
+            paramNames <- NA
+            paramNames2 <- NA
+            for(j in 1:length(runJags$estimates)){
+                  if(is.vector(runJags$credInterval[[j]])){
+                     paramNames <- c(paramNames, names(runJags$credInterval[j]))
+                  }else{
+                     paramNames <- c(paramNames, colnames(runJags$credInterval[[j]]))
+                  }
+                  if(length(design$parameter_set[[j]])==1){
+                    paramNames2 <- c(paramNames2, names(design$parameter_set[j]))
+                  }else{
+                    labels <- paste(names(design$parameter_set[j]), "[",1:length(design$parameter_set[[j]]),"]",sep="")
+                    paramNames2 <- c(paramNames2, labels)
+                  }
+            }
+            paramNames <- paramNames[-1]
+            paramNames2 <- paramNames2[-1]
+            colnames(MatEstimates) <- paramNames
+            colnames(ArrayCredInt) <- paramNames
+            colnames(MatTrueVal)   <- paramNames2
+            colnames(MatRhats) <- names(runJags$rhats)
+            
+            if(Show){check_Rhat(MatRhats)}
+            
+            output <- list("rhats"  = MatRhats, "estimates" = MatEstimates, "credIntervals" = ArrayCredInt,
+                           "trueValues" = MatTrueVal, "settings" = settings, "n.chains" = n.chains)
+            save(output, file=outputFile)
+            return(output)
+    }else{  cat("This simulation had been run before.\nLoading stored results: COMPLETE!")  
+            return(output)
+    }
+}
