d22fb48efe5345bd7614438b6cb6a9dbfe3ee0d0
Adriana Chavez <adrifelcha@gmail.com>
2024-06-18 19:53:52 -0700

    Add: The sample_data() function now generates data when we have participants go through different conditions (currently necessary for the within-groups comparison

--- code/functions/sample_data.R
+++ code/functions/sample_data.R
@@ -20,20 +20,25 @@ sample_data <- function(nPart, nTrials = NA, parameter_set, nTrialsPerCondition 
   }else{
         nObs <- nPart*nTrialsPerCondition*2
         data <- matrix(NA,ncol=4,nrow=nObs)
-        data[,1] <- rep(rep(1:nPart, each=nTrialsPerCondition),)
+        data[,1] <- rep(1:nPart, each=(nTrialsPerCondition*2))
+        data[,2] <- rep(1:0,nPart)
+        j = 1
         for(i in 1:nPart){      # Get data for every Participant
-          this.sub <- which(data[,1]==i)
-          accuracy = 0
-          while(sum(accuracy)==0){
-            temp <- generate_dataset(a = parameter_set$bound[i], v = parameter_set$drift[i], 
-                                     t = parameter_set$nondt[i], n = nTrials)
-            accuracy = temp$accuracy
-          }
-          data[this.sub,3] <- accuracy
-          data[this.sub,2] <- temp$RT
+            for(k in 1:0){
+                this.cell <- which(data[,1]==i&data[,2]==k)
+                accuracy = 0
+                while(sum(accuracy)==0){
+                  temp <- generate_dataset(a = parameter_set$bound[i], v = parameter_set$drift[j], 
+                                           t = parameter_set$nondt[i], n = nTrialsPerCondition)
+                  accuracy = temp$accuracy
+                }
+                data[this.cell,4] <- accuracy
+                data[this.cell,3] <- temp$RT
+                j = j+1
+            }
         }
         data <- as.matrix(data)
-        colnames(data) <- c("sub", "rt", "accuracy")    
+        colnames(data) <- c("sub","cond","rt", "accuracy")    
   }
   return(data)
 }
