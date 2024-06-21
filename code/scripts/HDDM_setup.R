ca6ef9f91d132fbf35b5578973fede7d042cbcc2
Adrifelcha <adrifelcha@gmail.com>
2024-06-21 11:31:24 -0700

    Add: Since the sample_parameters() function was updated to include a fixed Beta, we have to input a fixedBeta=NA argument now

--- code/scripts/HDDM_setup.R
+++ code/scripts/HDDM_setup.R
@@ -1,6 +1,6 @@
 HDDM_setup <-function(priors, nPart, nTrials, modelType=NA, X=NA, criterion=NA, fromPrior=TRUE, Show=TRUE){
     # Sample "true parameters" for the simulation using the priors
-    parameter_set <- sample_parameters(priors, nPart, modelType, X, criterion, fromPrior, Show)
+    parameter_set <- sample_parameters(priors, nPart, modelType, X, criterion, fromPrior, Show, fixedBeta = NA)
     # Generate data
     rawData = sample_data(nPart, nTrials, parameter_set)
     summData = getStatistics(rawData) 
