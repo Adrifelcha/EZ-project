fa79fdc6dcc44d250adedfe8b82b76daca0d3481
Adriana Chavez <adrifelcha@gmail.com>
2024-06-22 15:11:44 -0700

    Fix: Invert order in which designs are executed; do ttest and metaregression first

--- code/scripts/HDDM_runFullSeed.R
+++ code/scripts/HDDM_runFullSeed.R
@@ -17,28 +17,28 @@ HDDM_runFullSeed <- function(seed, settings){
           
           for(t in settings$trial_levels){
             
-              if(d=="hierarchical"){
+              if(d!="hierarchical"){
+                for(c in settings$criterion_levels){
+                  design <- HDDM_setup(settings$priors[[d]], p, t, d, X[,d], c, settings$fromPrior, Show = FALSE)
+                  runJags <- HDDM_runJAGS(summaryData = design$sumData, t, X[,d], settings$jagsData[[d]], 
+                                          settings$jagsParameters[[d]], settings$jagsInits[[as.character(p)]],
+                                          settings$n.chains, settings$modelFile[d,c], Show = FALSE)
+                  out_Beta <- rbind(out_Beta,list(seed = seed, p = p, t = t, d = d, c = c, rhats = runJags$rhats,
+                                                  true.values    = design$parameter_set, mean.estimates = runJags$estimates,
+                                                  std.estimates  = runJags$estimates, elapsed.time   = runJags$clock))
+                  cell <- 1 + cell
+                  cat("Iteration", cell, "of",  settings$nCells,"\n")
+                }
+              }else{
                 design <- HDDM_setup(settings$priors[[d]], p, t, d, X[,d], criterion = NA, settings$fromPrior, Show = FALSE)
                 runJags <- HDDM_runJAGS(summaryData = design$sumData, t, X[,d], settings$jagsData[[d]], 
                                         settings$jagsParameters[[d]], settings$jagsInits[[as.character(p)]],  
                                         settings$n.chains, settings$modelFile[d,1], Show = FALSE)
                 out_H <- rbind(out_H,list(seed = seed, p = p, t = t, d = d, rhats = runJags$rhats,
-                               true.values    = design$parameter_set, mean.estimates = runJags$estimates,
-                               std.estimates  = runJags$estimates, elapsed.time   = runJags$clock))
-                cell <- 1 + cell
-                cat("Iteration", cell, "of",  settings$nCells,"\n")
-              }else{
-                    for(c in settings$criterion_levels){
-                        design <- HDDM_setup(settings$priors[[d]], p, t, d, X[,d], c, settings$fromPrior, Show = FALSE)
-                        runJags <- HDDM_runJAGS(summaryData = design$sumData, t, X[,d], settings$jagsData[[d]], 
-                                                settings$jagsParameters[[d]], settings$jagsInits[[as.character(p)]],
-                                                settings$n.chains, settings$modelFile[d,c], Show = FALSE)
-                        out_Beta <- rbind(out_Beta,list(seed = seed, p = p, t = t, d = d, c = c, rhats = runJags$rhats,
                                           true.values    = design$parameter_set, mean.estimates = runJags$estimates,
                                           std.estimates  = runJags$estimates, elapsed.time   = runJags$clock))
-                        cell <- 1 + cell
-                        cat("Iteration", cell, "of",  settings$nCells,"\n")
-                    }
+                cell <- 1 + cell
+                cat("Iteration", cell, "of",  settings$nCells,"\n")
               }
           }
       }
