8a225f61c1f2a87c69663625f5487162545ddc67
Adriana Chavez <adrifelcha@gmail.com>
2024-06-23 13:52:17 -0700

    Take back last commit: Use single distribution for all designs

--- code/functions/default_inits.R
+++ code/functions/default_inits.R
@@ -1,13 +1,7 @@
 default_inits <- function(n.chains,nParticipants,modelType=NA){
   myinits <- rep(list(list()), n.chains)
-  if(modelType=="hierarchical"){
-      for(i in 1:n.chains){
-        myinits[[i]] <- list(drift = rnorm(nParticipants,0,0.4))
-      }
-  }else{
-      for(i in 1:n.chains){
-        myinits[[i]] <- list(drift = rnorm(nParticipants,0,1))
-      }
+  for(i in 1:n.chains){
+    myinits[[i]] <- list(drift = rnorm(nParticipants,0,0.5))
   }
   return(myinits)
 }
