9a54accebd7a77b6fd004ff8f843a2259a721ae9
Adriana Chavez <adrifelcha@gmail.com>
2024-06-22 18:37:21 -0700

    Add: Basic namingFunction now considers whether parameter values where generated from the prior or a uniform

--- code/functions/namingFunctions.R
+++ code/functions/namingFunctions.R
@@ -1,13 +1,20 @@
-nameOutput <- function(nTrials, nParticipants, nDatasets, modelType=NA){
+nameOutput <- function(nTrials, nParticipants, nDatasets, modelType=NA, fromPrior=NA){
   if(is.na(modelType)|modelType=="hierarchical"){
           fileName <- paste("./sim_P",nParticipants,"T",nTrials,"D",nDatasets,
-                            "_EZBHDDM.RData", sep="")
+                            "_EZBHDDM", sep="")
   }else{if(modelType=="metaregression"){
           fileName <- paste("./sim_P",nParticipants,"T",nTrials,"D",nDatasets,
-                            "_MetaRegEZBHDDM.RData", sep="")
+                            "_MetaRegEZBHDDM", sep="")
   }else{if(modelType=="ttest"){
           fileName <- paste("./sim_P",nParticipants,"T",nTrials,"D",nDatasets,
-                            "_ttestEZBHDDM.RData", sep="")
+                            "_ttestEZBHDDM", sep="")
   }else{  stop("Invalid modelType")  }}}
+  
+  if(fromPrior){
+        fileName <- paste(fileName,"_genPrior.RData", sep="")
+  }else{
+        fileName <- paste(fileName,"_genUnif.RData", sep="")
+  }
+  
   return(fileName)
 }
