ee2be493d0aa23ba0997699f2801be118e6a6927
Adriana Chavez <adrifelcha@gmail.com>
2024-06-22 18:36:07 -0700

    Fix: Change priors for the nondt_sdev, to make them cover a slightly larger range. Add: Comments that roughly explain what is the 95% density covered by the priors suggested for the mean parameters

--- code/functions/default_priors.R
+++ code/functions/default_priors.R
@@ -1,10 +1,10 @@
 default_priors <- function(Show=TRUE, modelType=NA){
-  prior <- data.frame("bound_mean_mean" = 1.50,  "bound_mean_sdev" = 0.20,
-                      "drift_mean_mean" = 0.00,  "drift_mean_sdev" = 0.50,
-                      "nondt_mean_mean" = 0.30,  "nondt_mean_sdev" = 0.06,
-                      "bound_sdev_lower" = 0.10, "bound_sdev_upper" = 0.40,
+  prior <- data.frame("bound_mean_mean" = 1.50,  "bound_mean_sdev" = 0.20, # 95% density between {1.1 , 1.89}
+                      "drift_mean_mean" = 0.00,  "drift_mean_sdev" = 0.50, # 95% density between {-0.98 , 0.98}
+                      "nondt_mean_mean" = 0.30,  "nondt_mean_sdev" = 0.06, # 98% density between {0.18, 0.41}
+                      "bound_sdev_lower" = 0.10, "bound_sdev_upper" = 0.40,   
                       "drift_sdev_lower" = 0.20, "drift_sdev_upper" = 0.40,
-                      "nondt_sdev_lower" = 0.05, "nondt_sdev_upper" = 0.25)
+                      "nondt_sdev_lower" = 0.025, "nondt_sdev_upper" = 0.25)
   if(modelType!="hierarchical"){
             prior$betaweight_lower = -2.00
             prior$betaweight_upper = 2.00
