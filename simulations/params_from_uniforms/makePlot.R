4258962e7b291bcaf823bb5558c57ab87ac8380c
Adriana Chavez <adrifelcha@gmail.com>
2024-07-05 13:16:33 -0700

    Correct: Change filenames

--- simulations/params_from_uniforms/makePlot.R
+++ simulations/params_from_uniforms/makePlot.R
@@ -1,68 +1,68 @@
 
 png(file = "../../figures/simMeta_drift_drift.png", width = 5, height = 5, units="in",res=300) # Width and height of the plot in inches
 par(bg=NA)
-makeSimStudyPlot("../../simulations/params_from_uniforms/results/simStudy_Meta_drift.RData", par="drift_mean")
+makeSimStudyPlot("../../simulations/params_from_uniforms/simStudy2/results/simStudy_Meta_drift.RData", par="drift_mean")
 dev.off()
 
 png(file = "../../figures/simMeta_bound_drift.png", width = 5, height = 5, units="in",res=300) # Width and height of the plot in inches
 par(bg=NA)
-makeSimStudyPlot("../../simulations/params_from_uniforms/results/simStudy_Meta_bound.RData", par="drift_mean")
+makeSimStudyPlot("../../simulations/params_from_uniforms/simStudy2/results/simStudy_Meta_bound.RData", par="drift_mean")
 dev.off()
 
 png(file = "../../figures/simMeta_nondt_drift.png", width = 5, height = 5, units="in",res=300) # Width and height of the plot in inches
 par(bg=NA)
-makeSimStudyPlot("../../simulations/params_from_uniforms/results/simStudy_Meta_nondt.RData", par="drift_mean")
+makeSimStudyPlot("../../simulations/params_from_uniforms/simStudy2/results/simStudy_Meta_nondt.RData", par="drift_mean")
 dev.off()
 
 ####################
 
 png(file = "../../figures/simMeta_drift_nondt.png", width = 5, height = 5, units="in",res=300) # Width and height of the plot in inches
 par(bg=NA)
-makeSimStudyPlot("../../simulations/params_from_uniforms/results/simStudy_Meta_drift.RData", par="nondt_mean")
+makeSimStudyPlot("../../simulations/params_from_uniforms/simStudy2/results/simStudy_Meta_drift.RData", par="nondt_mean")
 dev.off()
 
 png(file = "../../figures/simMeta_bound_nondt.png", width = 5, height = 5, units="in",res=300) # Width and height of the plot in inches
 par(bg=NA)
-makeSimStudyPlot("../../simulations/params_from_uniforms/results/simStudy_Meta_bound.RData", par="nondt_mean")
+makeSimStudyPlot("../../simulations/params_from_uniforms/simStudy2/results/simStudy_Meta_bound.RData", par="nondt_mean")
 dev.off()
 
 png(file = "../../figures/simMeta_nondt_nondt.png", width = 5, height = 5, units="in",res=300) # Width and height of the plot in inches
 par(bg=NA)
-makeSimStudyPlot("../../simulations/params_from_uniforms/results/simStudy_Meta_nondt.RData", par="nondt_mean")
+makeSimStudyPlot("../../simulations/params_from_uniforms/simStudy2/results/simStudy_Meta_nondt.RData", par="nondt_mean")
 dev.off()
 
 ####################
 
 png(file = "../../figures/simMeta_drift_bound.png", width = 5, height = 5, units="in",res=300) # Width and height of the plot in inches
 par(bg=NA)
-makeSimStudyPlot("../../simulations/params_from_uniforms/results/simStudy_Meta_drift.RData", par="bound_mean")
+makeSimStudyPlot("../../simulations/params_from_uniforms/simStudy2/results/simStudy_Meta_drift.RData", par="bound_mean")
 dev.off()
 
 png(file = "../../figures/simMeta_bound_bound.png", width = 5, height = 5, units="in",res=300) # Width and height of the plot in inches
 par(bg=NA)
-makeSimStudyPlot("../../simulations/params_from_uniforms/results/simStudy_Meta_bound.RData", par="bound_mean")
+makeSimStudyPlot("../../simulations/params_from_uniforms/simStudy2/results/simStudy_Meta_bound.RData", par="bound_mean")
 dev.off()
 
 png(file = "../../figures/simMeta_nondt_bound.png", width = 5, height = 5, units="in",res=300) # Width and height of the plot in inches
 par(bg=NA)
-makeSimStudyPlot("../../simulations/params_from_uniforms/results/simStudy_Meta_nondt.RData", par="bound_mean")
+makeSimStudyPlot("../../simulations/params_from_uniforms/simStudy2/results/simStudy_Meta_nondt.RData", par="bound_mean")
 dev.off()
 
 ####################
 
 png(file = "../../figures/simMeta_drift_beta.png", width = 5, height = 5, units="in",res=300) # Width and height of the plot in inches
 par(bg=NA)
-makeSimStudyPlot("../../simulations/params_from_uniforms/results/simStudy_Meta_drift.RData", par="betaweight")
+makeSimStudyPlot("../../simulations/params_from_uniforms/simStudy2/results/simStudy_Meta_drift.RData", par="betaweight")
 dev.off()
 
 png(file = "../../figures/simMeta_bound_beta.png", width = 5, height = 5, units="in",res=300) # Width and height of the plot in inches
 par(bg=NA)
-makeSimStudyPlot("../../simulations/params_from_uniforms/results/simStudy_Meta_bound.RData", par="betaweight")
+makeSimStudyPlot("../../simulations/params_from_uniforms/simStudy2/results/simStudy_Meta_bound.RData", par="betaweight")
 dev.off()
 
 png(file = "../../figures/simMeta_nondt_beta.png", width = 5, height = 5, units="in",res=300) # Width and height of the plot in inches
 par(bg=NA)
-makeSimStudyPlot("../../simulations/params_from_uniforms/results/simStudy_Meta_nondt.RData", par="betaweight")
+makeSimStudyPlot("../../simulations/params_from_uniforms/simStudy2/results/simStudy_Meta_nondt.RData", par="betaweight")
 dev.off()
 
 
@@ -73,66 +73,66 @@ dev.off()
 
 png(file = "../../figures/simTtest_drift_drift.png", width = 5, height = 5, units="in",res=300) # Width and height of the plot in inches
 par(bg=NA)
-makeSimStudyPlot("../../simulations/params_from_uniforms/results/simStudy_Ttest_drift.RData", par="drift_mean")
+makeSimStudyPlot("../../simulations/params_from_uniforms/simStudy2/results/simStudy_Ttest_drift.RData", par="drift_mean")
 dev.off()
 
 png(file = "../../figures/simTtest_bound_drift.png", width = 5, height = 5, units="in",res=300) # Width and height of the plot in inches
 par(bg=NA)
-makeSimStudyPlot("../../simulations/params_from_uniforms/results/simStudy_Ttest_bound.RData", par="drift_mean")
+makeSimStudyPlot("../../simulations/params_from_uniforms/simStudy2/results/simStudy_Ttest_bound.RData", par="drift_mean")
 dev.off()
 
 png(file = "../../figures/simTtest_nondt_drift.png", width = 5, height = 5, units="in",res=300) # Width and height of the plot in inches
 par(bg=NA)
-makeSimStudyPlot("../../simulations/params_from_uniforms/results/simStudy_Ttst_nondt.RData", par="drift_mean")
+makeSimStudyPlot("../../simulations/params_from_uniforms/simStudy2/results/simStudy_Ttest_nondt.RData", par="drift_mean")
 dev.off()
 
 ####################
 
 png(file = "../../figures/simTtest_drift_nondt.png", width = 5, height = 5, units="in",res=300) # Width and height of the plot in inches
 par(bg=NA)
-makeSimStudyPlot("../../simulations/params_from_uniforms/results/simStudy_Ttest_drift.RData", par="drift_mean")
+makeSimStudyPlot("../../simulations/params_from_uniforms/simStudy2/results/simStudy_Ttest_drift.RData", par="nondt_mean")
 dev.off()
 
 png(file = "../../figures/simTtest_bound_nondt.png", width = 5, height = 5, units="in",res=300) # Width and height of the plot in inches
 par(bg=NA)
-makeSimStudyPlot("../../simulations/params_from_uniforms/results/simStudy_Ttest_bound.RData", par="drift_mean")
+makeSimStudyPlot("../../simulations/params_from_uniforms/simStudy2/results/simStudy_Ttest_bound.RData", par="nondt_mean")
 dev.off()
 
 png(file = "../../figures/simTtest_nondt_nondt.png", width = 5, height = 5, units="in",res=300) # Width and height of the plot in inches
 par(bg=NA)
-makeSimStudyPlot("../../simulations/params_from_uniforms/results/simStudy_Ttst_nondt.RData", par="drift_mean")
+makeSimStudyPlot("../../simulations/params_from_uniforms/simStudy2/results/simStudy_Ttest_nondt.RData", par="nondt_mean")
 dev.off()
 
 ####################
 
 png(file = "../../figures/simTtest_drift_bound.png", width = 5, height = 5, units="in",res=300) # Width and height of the plot in inches
 par(bg=NA)
-makeSimStudyPlot("../../simulations/params_from_uniforms/results/simStudy_Ttest_drift.RData", par="drift_mean")
+makeSimStudyPlot("../../simulations/params_from_uniforms/simStudy2/results/simStudy_Ttest_drift.RData", par="bound_mean")
 dev.off()
 
 png(file = "../../figures/simTtest_bound_bound.png", width = 5, height = 5, units="in",res=300) # Width and height of the plot in inches
 par(bg=NA)
-makeSimStudyPlot("../../simulations/params_from_uniforms/results/simStudy_Ttest_bound.RData", par="drift_mean")
+makeSimStudyPlot("../../simulations/params_from_uniforms/simStudy2/results/simStudy_Ttest_bound.RData", par="bound_mean")
 dev.off()
 
 png(file = "../../figures/simTtest_nondt_bound.png", width = 5, height = 5, units="in",res=300) # Width and height of the plot in inches
 par(bg=NA)
-makeSimStudyPlot("../../simulations/params_from_uniforms/results/simStudy_Ttst_nondt.RData", par="drift_mean")
+makeSimStudyPlot("../../simulations/params_from_uniforms/simStudy2/results/simStudy_Ttest_nondt.RData", par="bound_mean")
 dev.off()
 
 ####################
 
 png(file = "../../figures/simTtest_drift_beta.png", width = 5, height = 5, units="in",res=300) # Width and height of the plot in inches
 par(bg=NA)
-makeSimStudyPlot("../../simulations/params_from_uniforms/results/simStudy_Ttest_drift.RData", par="betaweight")
+makeSimStudyPlot("../../simulations/params_from_uniforms/simStudy2/results/simStudy_Ttest_drift.RData", par="betaweight")
 dev.off()
 
 png(file = "../../figures/simTtest_bound_beta.png", width = 5, height = 5, units="in",res=300) # Width and height of the plot in inches
 par(bg=NA)
-makeSimStudyPlot("../../simulations/params_from_uniforms/results/simStudy_Ttest_bound.RData", par="betaweight")
+makeSimStudyPlot("../../simulations/params_from_uniforms/simStudy2/results/simStudy_Ttest_bound.RData", par="betaweight")
 dev.off()
 
 png(file = "../../figures/simTtest_nondt_beta.png", width = 5, height = 5, units="in",res=300) # Width and height of the plot in inches
 par(bg=NA)
-makeSimStudyPlot("../../simulations/params_from_uniforms/results/simStudy_Ttst_nondt.RData", par="betaweight")
+makeSimStudyPlot("../../simulations/params_from_uniforms/simStudy2/results/simStudy_Ttest_nondt.RData", par="betaweight")
 dev.off()
