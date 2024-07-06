6060cdfd01b32f4369cdddd67c539a0315b8a31f
Adriana Chavez <adrifelcha@gmail.com>
2024-07-05 20:15:02 -0700

    Change default priors: Still use normal distributions for the hierarchical means, but make them so that 99% of density covers the ranges used for the uniform sampling distributions. Change the uniform distributions used as priors for all hierarchical standard deviations so they are a little larger than the values estimated

--- code/functions/default_priors.R
+++ code/functions/default_priors.R
@@ -1,10 +1,16 @@
 default_priors <- function(Show=TRUE, modelType=NA){
-  prior <- data.frame("bound_mean_mean" = 1.50,  "bound_mean_sdev" = 0.20, # 95% density between {1.1 , 1.89}
-                      "drift_mean_mean" = 0.00,  "drift_mean_sdev" = 0.50, # 95% density between {-0.98 , 0.98}
-                      "nondt_mean_mean" = 0.30,  "nondt_mean_sdev" = 0.06, # 98% density between {0.18, 0.41}
-                      "bound_sdev_lower" = 0.10, "bound_sdev_upper" = 0.40,   
-                      "drift_sdev_lower" = 0.20, "drift_sdev_upper" = 0.40,
-                      "nondt_sdev_lower" = 0.025, "nondt_sdev_upper" = 0.25)
+  prior <- data.frame("bound_mean_mean" = 2.25,  "bound_mean_sdev" = 0.80, # 99% density between {0.38, 4.11}
+                      "drift_mean_mean" = 0.00,  "drift_mean_sdev" = 2.15, # 99% density between {-5 , 5}
+                      "nondt_mean_mean" = 0.55,  "nondt_mean_sdev" = 0.20, # 99% density between {0.08, 1.01}
+                      "bound_sdev_lower" = 0.05, "bound_sdev_upper" = 1,   
+                      "drift_sdev_lower" = 0.05, "drift_sdev_upper" = 2,
+                      "nondt_sdev_lower" = 0.01, "nondt_sdev_upper" = 0.25)
+  # prior <- data.frame("bound_mean_mean" = 1.50,  "bound_mean_sdev" = 0.20, # 95% density between {1.1 , 1.89}
+  #                     "drift_mean_mean" = 0.00,  "drift_mean_sdev" = 0.50, # 95% density between {-0.98 , 0.98}
+  #                     "nondt_mean_mean" = 0.30,  "nondt_mean_sdev" = 0.06, # 98% density between {0.18, 0.41}
+  #                     "bound_sdev_lower" = 0.10, "bound_sdev_upper" = 0.40,   
+  #                     "drift_sdev_lower" = 0.20, "drift_sdev_upper" = 0.40,
+  #                     "nondt_sdev_lower" = 0.025, "nondt_sdev_upper" = 0.25)
   if(modelType!="hierarchical"){
             prior$betaweight_mean = 0
             prior$betaweight_sdev = 1
