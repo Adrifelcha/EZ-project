88df6d9e2e1804a1cb1fbedf1004037f956c8561
Adriana Chavez <adrifelcha@gmail.com>
2024-07-11 14:11:05 -0700

    Major change: Instead of using a uniform prior on the hierarchical standard deviations, use a gamma distribution as a prior for inverse-sigma

--- code/functions/default_priors.R
+++ code/functions/default_priors.R
@@ -1,10 +1,10 @@
 default_priors <- function(Show=TRUE, modelType=NA){
-  prior <- data.frame("bound_mean_mean" = 2.25,  "bound_mean_sdev" = 0.80, # 99% density between {0.38, 4.11}
-                      "drift_mean_mean" = 0.00,  "drift_mean_sdev" = 2.15, # 99% density between {-5 , 5}
-                      "nondt_mean_mean" = 0.55,  "nondt_mean_sdev" = 0.20, # 99% density between {0.08, 1.01}
-                      "bound_sdev_lower" = 0.05, "bound_sdev_upper" = 1,   
-                      "drift_sdev_lower" = 0.05, "drift_sdev_upper" = 2,
-                      "nondt_sdev_lower" = 0.01, "nondt_sdev_upper" = 0.25)
+  prior <- data.frame("bound_mean_mean" = 2.25,  "bound_mean_sdev" = 1.00, # 99% density between {-0.07, 4.57}
+                      "drift_mean_mean" = 0.00,  "drift_mean_sdev" = 3.00, # 99% density between {-6.9 , 6.9}
+                      "nondt_mean_mean" = 0.55,  "nondt_mean_sdev" = 0.25, # 99% density between {-0.03, 1.13}
+                      "bound_sdev_shape" = 3.00,  "bound_sdev_rate" = 0.50,   
+                      "drift_sdev_shape" = 2.00,  "drift_sdev_rate" = 0.20,
+                      "nondt_sdev_shape" = 4.00,  "nondt_sdev_rate" = 0.25)
   # prior <- data.frame("bound_mean_mean" = 1.50,  "bound_mean_sdev" = 0.20, # 95% density between {1.1 , 1.89}
   #                     "drift_mean_mean" = 0.00,  "drift_mean_sdev" = 0.50, # 95% density between {-0.98 , 0.98}
   #                     "nondt_mean_mean" = 0.30,  "nondt_mean_sdev" = 0.06, # 98% density between {0.18, 0.41}
