4f028ef970b69ab3a4ddc06f91363398583c74b6
Adriana Chavez <adrifelcha@gmail.com>
2024-07-11 14:08:38 -0700

    Change default inits: Use a wider normal for the individual drift init values. Add (Commented) lines to specify other init distributions. These values have been tested in an attempt to increase chain convergence

--- code/functions/default_inits.R
+++ code/functions/default_inits.R
@@ -1,7 +1,14 @@
 default_inits <- function(n.chains,nParticipants,modelType=NA){
   myinits <- rep(list(list()), n.chains)
   for(i in 1:n.chains){
-    myinits[[i]] <- list(drift = rnorm(nParticipants,0,1))
+    myinits[[i]] <- list(drift = rnorm(nParticipants,0,0.5))
+                         #bound = rnorm(nParticipants,2.25,0.4))
+                         #bound_mean = 2.25,
+                         #drift_mean = 0,
+                         #nondt_mean = 0.55)
+                         #drift_sdev = 0.5,
+                         #bound_sdev = 0.4,
+                         #nondt_sdev = 0.11)
   }
   return(myinits)
 }
