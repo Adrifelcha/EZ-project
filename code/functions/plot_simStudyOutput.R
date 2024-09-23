5b74da302095c99c98b0b5a18bf2a86add0fdb93
Adriana Chavez <adrifelcha@gmail.com>
2024-09-23 01:31:05 -0700

    Finish up function to plot simulation study results with relevant quantiles to display variance and bias

--- code/functions/plot_simStudyOutput.R
+++ code/functions/plot_simStudyOutput.R
@@ -1,49 +1,60 @@
-makeSimStudyPlot <- function(simStudyRData, param=NA){
+makeSimStudyPlot <- function(simStudyRData, param=NA, plotType=1, plot.range=NA){
   assign('obj', get(load(simStudyRData)))
   lvls <- c(20,40,80,160,320)
+  nL <- length(lvls)
+  print_Yaxis <- rep(c(TRUE,rep(FALSE,nL-1)),nL)
+  print_Xaxis <- c(rep(rep(FALSE,nL),nL-1),rep(TRUE,nL))
   P <- lvls
   Tr <- lvls
-  if(is.na(param)){   
+  if(sum(is.na(param))>0){   
         param <- c("drift_mean", "nondt_mean", "bound_mean")    
         if(grepl("Meta", simStudyRData)|grepl("Ttest", simStudyRData)){
            param <- c(param,"betaweight")
         }
   }
   
-    for(par in param){
-        if(par=="drift_mean"){plot.range <- c(-5,5)}
-        if(par=="nondt_mean"){plot.range <- c(0.2,0.95)}
-        if(par=="bound_mean"){plot.range <- c(0.5,4)}
-        if(par=="betaweight"){
-              if(grepl("nondt", simStudyRData)){  plot.range <- c(0,1)  
-              }else{   plot.range <- c(-1,1)} 
+  for(par in param){
+      par(pty="s", mfrow=c(5,5), mai=c(0,0,0.05,0.05), 
+          oma=c(2,1.5,1.5,1.5))
+      panel_no <- 1
+      print(par)
+      for(i in 1:5){
+            thisP_x <- obj$true[[i]]
+            thisP_y <- obj$recovered[[i]]
+        for(k in 1:5){
+                this.panel <- list("estimates" = matrix(as.vector(thisP_x[,par,k]), ncol=1,
+                                                        dimnames = list(list(),par)),
+                                   "trueValues" = matrix(as.vector(thisP_y[,par,k]), ncol=1,
+                                                         dimnames = list(list(),par)))
+            if(plotType==1){
+                make_panel_type1(this.panel, parameter=par, 
+                             add.titles = FALSE, plot.range=plot.range)
+            }else{
+                make_panel_type2(this.panel, parameter=par, 
+                             add.titles = FALSE, nBins=15, 
+                             plot.range=plot.range,
+                             axisX = print_Xaxis[panel_no], 
+                             axisY = print_Yaxis[panel_no])
+            }
+            box(lty=3)
+            if(k==1){mtext(paste("P =",P[i]),2,line=3,f=2)}
+            if(i==1){mtext(paste("T =",Tr[k]),line=0.5,f=2)}
+            panel_no <- panel_no +1
+            
+            print(paste("k =", k, " and i =", i, sep=""))
+            print(cbind(this.panel$estimates[1:2], this.panel$trueValues[1:2]))
         }
-        fixed.axis <- seq(plot.range[1],plot.range[2],length.out=2)
-    
-        par(pty="s", mfrow=c(5,5), mai=c(0,0,0.05,0.05), oma=c(2,1.5,1.5,1.5))
-        for(i in 1:5){
-              thisP_x <- obj$true[[i]]
-              thisP_y <- obj$recovered[[i]]
-          for(k in 1:5){
-              x <- thisP_x[,param,k]
-              y <- thisP_y[,param,k]
-              plot(5,5, xlim=plot.range, ylim=plot.range, ann=F, axes=F, col="#3480C5")
-              abline(v=mean(plot.range),lty=3,col="gray60")
-              abline(h=mean(plot.range),lty=3,col="gray60")
-              #mtext(paste(par), outer = TRUE, line=-20, cex=3, col="gray90")
-              points(x,y,col="#3480C5",pch=16)
-              box(lty=3)
-              abline(0,1,lty=3,lwd=3,col="gray20")
-              if(k==1){mtext(paste("P =",P[i]),2,line=0.5,f=2)}
-              if(i==1){mtext(paste("T =",Tr[k]),line=0.5,f=2)}
-              if(par=="bound_mean"){
-                  if(i==5){axis(1,fixed.axis,round(fixed.axis,0))}
-                  if(k==5){axis(4,fixed.axis,round(fixed.axis,0), las=2)}
-              }else{
-                  if(i==5){axis(1,fixed.axis,round(fixed.axis,1))}
-                  if(k==5){axis(4,fixed.axis,round(fixed.axis,1), las=2)}
-              }
-          }
-        }
-    }
+      }
+      if(par=="drift_mean"){       label <-  expression(paste(mu[nu]))   }
+      if(par=="nondt_mean"){       label <-  expression(paste(mu[tau]))   }
+      if(par=="bound_mean"){       label <-  expression(paste(mu[alpha]))   }
+      if(par=="betaweight"){  label <-  expression(paste(beta))   }
+      mtext(label, 4, outer=T, las=2, line=-2, cex=2)
+      
+  }
 }
+
+simStudyRData <- "../../simStudy1/results/simStudy_Meta_bound.RData"
+#dev.new(width=8, height=12)
+makeSimStudyPlot(simStudyRData, param=NA, plotType=2)
+  
