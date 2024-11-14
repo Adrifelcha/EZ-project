c7240ff8b79f7f06405f289f7ed0fda189561d55
Adriana Chavez <adrifelcha@gmail.com>
2024-11-13 20:04:13 -0800

    Add: A logical argument to determine whether to include the design and criterion used for the simulation study as a title / Add: A logical argument to print to screen the plots being generated, just to double check

--- code/functions/plot_simStudyOutput.R
+++ code/functions/plot_simStudyOutput.R
@@ -1,4 +1,4 @@
-makeSimStudyPlot <- function(simStudyRData, param=NA, plotType=1, plot.range=NA, showParam = TRUE, showStudy = FALSE){
+makeSimStudyPlot <- function(simStudyRData, param=NA, plotType=1, plot.range=NA, showParam = TRUE, showStudy = FALSE, showTestValues=FALSE){
   assign('obj', get(load(simStudyRData)))
   lvls <- c(20,40,80,160,320)
   nL <- length(lvls)
@@ -15,18 +15,18 @@ makeSimStudyPlot <- function(simStudyRData, param=NA, plotType=1, plot.range=NA,
   
   for(par in param){
       mai <- c(0,0,0.075,0.075)
-      if(showParam){
-                      par(pty="s", mfrow=c(5,5), mai=mai, oma=c(2.5,1.75,1,4.7))
-      }else{
-                      par(pty="s", mfrow=c(5,5), mai=mai, oma=c(2.5,1.75,1.4,1.7))
-      }
-      panel_no <- 1
+      right_margin <- 1.7 
+      upper_margin <- 1
+      if(showParam){ right_margin <- 4.7  }
+      if(showStudy){ upper_margin <- 3.7    }
+      par(pty="s", mfrow=c(5,5), mai=mai, oma=c(2.5,1.75,upper_margin,right_margin))
       
       if(sum(is.na(plot.range))>0){    plot.range <- round(range(c(obj$true[[1]][,par,]
                                                             ,obj$recovered[[1]][,par,]), na.rm = TRUE),1)
       }
       
       print(par)
+      panel_no <- 1
       for(i in 1:5){
             thisP_x <- obj$true[[i]]
             thisP_y <- obj$recovered[[i]]
@@ -50,8 +50,12 @@ makeSimStudyPlot <- function(simStudyRData, param=NA, plotType=1, plot.range=NA,
             if(i==1){mtext(paste("T =",Tr[k]),line=0.3,f=2)}
             panel_no <- panel_no +1
             
-            print(paste("k =", k, " and i =", i, sep=""))
-            print(cbind(this.panel$estimates[1:2], this.panel$trueValues[1:2]))
+            print(paste("P =", i, " and T =", k, sep=""))
+            if(showTestValues){
+                screenTest <- round(cbind(this.panel$estimates[1:2], this.panel$trueValues[1:2]),5)
+                colnames(screenTest) <- c("Estimates", "True")
+                print(screenTest)
+            }
         }
       }
       if(showParam){
@@ -61,11 +65,29 @@ makeSimStudyPlot <- function(simStudyRData, param=NA, plotType=1, plot.range=NA,
               if(par=="betaweight"){  label <-  expression(paste(beta))   }
               mtext(label, 4, outer=T, las=2, line=1.75, cex=2)
       }
+      if(showStudy){
+        fileName <- simStudyRData
+        remove.RData <- gsub(".RData","",fileName)
+        split_simStudyID <- strsplit(remove.RData, "Study_")
+        studyInfo <- split_simStudyID[[1]][2]
+        split_design <- unlist(strsplit(studyInfo, "_"))
+        if(split_design[1]=="Meta"|split_design[1]=="Ttest"){
+          if(split_design[1]=="Meta"){ 
+            design_label <- "Metaregression"}
+            else{                       
+              design_label <- "T-test design"
+            }
+          if(split_design[2]=="drift"){ title <-  bquote(.(design_label) ~ "on" ~ nu)   }
+          if(split_design[2]=="bound"){ title <-  bquote(.(design_label) ~ "on" ~ alpha)   }
+          if(split_design[2]=="nondt"){ title <-  bquote(.(design_label) ~ "on" ~ tau)   }
+        }
+        mtext(title, 3, outer=T, las=1, line=1.2, cex=1.6, f=2)
+      }
   }
 }
 
-#check.par <- "drift_mean"
-#simStudyRData <- "./results/simStudy_Meta_drift.RData"
-#makeSimStudyPlot(simStudyRData, param=check.par, plotType=2, plot.range=NA, showParam = TRUE, showStudy = FALSE)
+check.par <- "drift_mean"
+simStudyRData <- "./results/simStudy_Meta_drift.RData"
+makeSimStudyPlot(simStudyRData, param=check.par, plotType=2, plot.range=NA, showParam = TRUE, showStudy = TRUE)
 #source("../../simulations/params_from_uniforms/simStudy1/figures/plot_simStudyOutput_outdated.R")
 #makeSimStudyPlot(simStudyRData, param=check.par)
