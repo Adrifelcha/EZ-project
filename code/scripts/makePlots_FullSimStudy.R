4ed2aa3ba0cbeb6b9ab42e55f946a367123958be
Adriana Chavez <adrifelcha@gmail.com>
2024-11-28 00:19:05 -0800

    Minor changes: Update the range of values displayed in the simulation studies with generative uniforms / Major changes: Script now generates pdf files as well

--- code/scripts/makePlots_FullSimStudy.R
+++ code/scripts/makePlots_FullSimStudy.R
@@ -5,25 +5,27 @@
 #simulationDirectory <- "../../simulations/generative_priors/"
 #setwd(simulationDirectory)
 results.at <- "./results/"
-save.to <- "../../figures/"
-simStudyID <- "genPrior"
-showParam = TRUE
+save.to <- "../../../figures/"
+simStudyID <- "unifPrior"
+showParam = FALSE
+showStudy = TRUE
 plotType = 2
 h = 5 #height in inches
 w = h #width in inches
 res = 300
 units <- "in"
+nBins <- 11
 
 ##### Simulaion study 1: Generative prior
-plot.ranges <- rbind(c(-1.3,1.3),   # drift
-                     c(1,2),        # bound
-                     c(0.15,0.4),   # nondt
-                     c(-1,1))       # betaweight
+#plot.ranges <- rbind(c(-1.3,1.3),   # drift
+#                     c(1,2),        # bound
+#                     c(0.15,0.4),   # nondt
+#                     c(-1,1))       # betaweight
 ###### Simulation study 2: Uniform parameters
-#plot.ranges <- rbind(c(-6,6),   # drift
-#                     c(0.25,5),   # bound
-#                     c(0.1,1.1),   # nondt
-#                     c(-1.5,1.5))   # betaweight
+plot.ranges <- rbind(c(-5.5,5.5),   # drift
+                     c(0.5,4),   # bound
+                     c(0.15,0.55),   # nondt
+                     c(-1,1))   # betaweight
 
 ###############################################################################
 # Make plots
@@ -46,7 +48,15 @@ for(d in designs.available){
             makePNG <- paste(save.to,simStudyID,d,"_",c,"_",parameters[i,1],".png",sep="")
             png(file = makePNG, width = w, height = h, units=units,res=res) 
             par(bg=NA)
-            makeSimStudyPlot(simResults, param=parameters[i,2], plotType=plotType, showParam=showParam, plot.range=plot.range[i,])
+            makeSimStudyPlot(simResults, param=parameters[i,2], plotType=plotType, showParam=showParam, 
+                             plot.range=plot.range[i,], showStudy=showStudy, nBins=nBins)
+            dev.off()
+            
+            makePDF <- paste(save.to,simStudyID,d,"_",c,"_",parameters[i,1],".pdf",sep="")
+            pdf(file = makePDF, width = w, height = h) 
+            par(bg=NA)
+            makeSimStudyPlot(simResults, param=parameters[i,2], plotType=plotType, showParam=showParam, 
+                             plot.range=plot.range[i,], showStudy=showStudy, nBins=nBins)
             dev.off()
         }
       
