ba1ee3a8e534359c2f589564df840478bfd01a5d
Adriana Chavez <adrifelcha@gmail.com>
2024-12-03 15:10:20 -0800

    Script used to generate the figure in the paper

--- simulations/hypothesis_testing/scripts/plot_hypothesisTesting_paper.R
+++ simulations/hypothesis_testing/scripts/plot_hypothesisTesting_paper.R
@@ -1,15 +1,13 @@
-#assign('B', get(load("../sim_P40Tc160D1000_FixedEffectNull.RData")))
-#assign('B1', get(load("../sim_P40Tc160D1000_FixedEffectDiff1.RData")))
-#assign('B2', get(load("../sim_P40Tc160D1000_FixedEffectDiff2.RData")))
-#assign('B3', get(load("../sim_P40Tc160D1000_FixedEffectDiff3.RData")))
-#assign('B4', get(load("../sim_P40Tc160D1000_FixedEffectDiff4.RData")))
-#assign('B5', get(load("../sim_P40Tc160D1000_FixedEffectDiff5.RData")))
+assign('B', get(load("./results_40x40/simStudy_B.RData")))
+assign('B2', get(load("./results_40x40/simStudy_B1.RData")))
+assign('B4', get(load("./results_40x40/simStudy_B2.RData")))
+
 #estimates <- list("B" = B, "B1" = B1, "B2" = B2, "B3" = B3, "B4" = B4, "B5" = B5)
-estimates <- list("B" = B, "B1" = B1, "B2" = B2, "B3" = B3, "B4" = B4, "B5" = B5)
+estimates <- list("B" = B, "B2" = B2, "B4" = B4)
 look.at <- c("B", "B2", "B4")
 levelsM <- 1000
 m <- seq(0,2,length.out=levelsM)
-m2 <- seq(1,3,length.out=levelsM)
+m2 <- seq(0,30,length.out=levelsM)
 p <- matrix(NA,ncol=length(estimates),nrow=levelsM)
 p2 <- p
 BF_crit <- 0.2
@@ -18,7 +16,7 @@ BF <- c()
 count <- 1
 for(b in look.at){
     getB <- estimates[[b]]
-    x <- getB$estimates[,"betaweight"]
+    x <- getB$recovered[,"betaweight"]
     sd <- sqrt(getB$variance[,"betaweight"])
     check <- x/sd
     betas <- getB$beta_chain
