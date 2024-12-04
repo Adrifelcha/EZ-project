34c34c7c98da4ebd05775b3972fe219529cf850f
Adriana Chavez <adrifelcha@gmail.com>
2024-12-04 01:01:25 -0800

    Add: An output to specify the folder where we wish to locate output files

--- code/functions/namingFunctions.R
+++ code/functions/namingFunctions.R
@@ -1,12 +1,16 @@
-nameOutput <- function(nTrials, nParticipants, nDatasets, modelType=NA, fromPrior=NA){
+nameOutput <- function(nTrials, nParticipants, nDatasets, modelType=NA, fromPrior=NA, output.folder = NA){
+  if(is.na(output.folder)){       output.folder <- "./"        }
+  
+  start <- paste(output.folder,"sim_P", sep="")
+  
   if(is.na(modelType)|modelType=="hierarchical"){
-          fileName <- paste("./sim_P",nParticipants,"T",nTrials,"D",nDatasets,
+          fileName <- paste(start,nParticipants,"T",nTrials,"D",nDatasets,
                             "_EZBHDDM", sep="")
   }else{if(modelType=="metaregression"){
-          fileName <- paste("./sim_P",nParticipants,"T",nTrials,"D",nDatasets,
+          fileName <- paste(start,nParticipants,"T",nTrials,"D",nDatasets,
                             "_MetaRegEZBHDDM", sep="")
   }else{if(modelType=="ttest"){
-          fileName <- paste("./sim_P",nParticipants,"T",nTrials,"D",nDatasets,
+          fileName <- paste(start,nParticipants,"T",nTrials,"D",nDatasets,
                             "_ttestEZBHDDM", sep="")
   }else{  stop("Invalid modelType")  }}}
   
