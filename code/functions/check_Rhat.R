dea101116d2ba196ce49b92423962509fec49d21
Adriana Chavez <adrifelcha@gmail.com>
2024-12-05 20:39:06 -0800

    Adjust function: 1) Don't take the deviance rhat into account; 2) Display results differently depending on the user choosing to explore all parameters vs only hierarchical means

--- code/functions/check_Rhat.R
+++ code/functions/check_Rhat.R
@@ -1,6 +1,7 @@
 # A function to check the Rhats for a simulation
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~!
 check_Rhat <- function(rhats){
+  if("deviance" %in% colnames(rhats)){ rhats <- rhats[,colnames(rhats) != "deviance"]}
   rule <- 1.05
   bad.Rhat <- which(rhats>rule, arr.ind = TRUE)
   test.rhat <- nrow(bad.Rhat) > 0
@@ -12,6 +13,10 @@ check_Rhat <- function(rhats){
           legend("top",paste("Rhat > ",rule," | ",
                              (round(nrow(bad.Rhat)/(length(as.vector(rhats))),5))*100,
                              "% of chains | ", length(which.are.bad.Rhats), " chains", sep=""), lty=2, col="red", cex=0.4)
-          table(which.are.bad.Rhats)
-  }else{      paste("No Rhat greater than ", rule, sep="")       }
+          print(table(which.are.bad.Rhats))
+  }else{    cat("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~!!!\n")
+            if(ncol(rhats)==3){      cat("R-hat check (Hierarchical mean parameters):\n")  
+            }else{                   cat("R-hat check (All parameters):\n")        }
+            cat(paste("No Rhat greater than", rule, "\n"))       
+            cat("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~!!!\n")  }
 }
