f10b595e4cdd916da30fad0b4dc235b12f415e40
Adriana Chavez <adrifelcha@gmail.com>
2024-12-09 12:31:48 -0800

    Adjust the grid (2x3 or 2x2) depending on whether we have included all hierarchical parameters or just the means

--- code/functions/plot_Chain.R
+++ code/functions/plot_Chain.R
@@ -1,12 +1,14 @@
 # A function to plot the merging chains for hierarchical parameters
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~!
-plot_Chain <- function(samples){
+plot_Chain <- function(samples, track_allParameters){
   posterior.samples <- samples$BUGSoutput$sims.array
   labels <- names(posterior.samples[1,1,])
   locateHier <- which(grepl("_",labels))
   N <- length(locateHier)
   n.chains <- ncol(posterior.samples[,,labels[locateHier[1]]])
-  par(mfrow = c(2, 3), mar =c(5.1,2,4.1,2), bty = "o")
+  if(track_allParameters){
+           par(mfrow = c(2, 3), mar =c(5.1,2,4.1,2), bty = "o")
+  }else{   par(mfrow = c(2, 2), mar =c(1,1,1,1), mai=c(0.5,0.5,0.5,0.2), bty = "o")     }
   for(i in locateHier){
     plot(posterior.samples[,1,i], type="l", main=labels[i], 
          xlab="Iteration", ylab="Value sampled")  
