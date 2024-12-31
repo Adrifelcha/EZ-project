492bd42a37fc8dd7a810297d51fadf06fdef7e2f
Adriana Chavez <adrifelcha@gmail.com>
2024-12-31 00:54:18 -0800

    Deleted files

--- code/scripts/recover_samples.R
+++
@@ -1,9 +0,0 @@
-setwd("../params_from_uniforms/simStudy_Nov24/samples/")
-
-recover_output <- list()
-for(archive in dir()){
-  print(archive)
-  load(archive)
-  recover_output <- rbind(recover_output,output)
-  }
-
