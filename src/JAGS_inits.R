5bd34eaceb0e0c05a292a8bc7c4c3cdab6b4ff46
Adriana Chavez <adrifelcha@gmail.com>
2025-03-13 16:31:49 -0700

    Add: custom_sd as an input allowing users to specify the sd of the Normal generating the initial values for the drift rate. Add header and comments.

--- src/JAGS_inits.R
+++ src/JAGS_inits.R
@@ -1,7 +1,16 @@
-default_inits <- function(n.chains,nParticipants,modelType=NA){
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~!
+# This function generates initial values for the individual drift rates
+# to pass to JAGS.
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~!
+JAGS_inits <- function(n.chains, nParticipants, custom_sd=0.3){
+  # Create a list to hold initial values for each chain
   myinits <- rep(list(list()), n.chains)
+  
+  # For each chain, generate random initial values
   for(i in 1:n.chains){
-    myinits[[i]] <- list(drift = rnorm(nParticipants,0,0.3))
+    # Initialize the `drift` parameter MCMC chain for each participant
+    myinits[[i]] <- list(drift = rnorm(nParticipants, 0, custom_sd))
   }
+
   return(myinits)
 }
