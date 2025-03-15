32b10c001091643830297182908f4f97638a6ca3
Adriana Chavez <adrifelcha@gmail.com>
2025-03-14 19:59:10 -0700

    Re-write function to update fileName coding. Also, create directory if needed

--- src/store_namingFunctions.R
+++ src/store_namingFunctions.R
@@ -2,38 +2,36 @@
 # This function generates standardized filenames for simulation output files
 # Note: This function ISN'T made to work for within-subject designs
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~!
-nameOutput <- function(nTrials, nParticipants, nDatasets, modelType=NA, fromPrior=NA, output.folder = NA){
-  # Set default output folder to current directory if not specified
-  if(is.na(output.folder)){
-    output.folder <- here("output")
-  }
-  
-  # Filename should specify the number of participants and trials, and the number of datasets
-  start <- paste("sim_P", nParticipants,"T", nTrials,"D", nDatasets, sep="")
-  
-  # Filename should specify the model type
-  if(is.na(modelType)|modelType=="hierarchical"){
-          # Basic hierarchical model
-          fileName <- paste(start, "_EZBHDDM", sep="")
-  }else{if(modelType=="metaregression"){
-          # Meta-regression model
-          fileName <- paste(start, "_MetaRegEZBHDDM", sep="")
-  }else{if(modelType=="ttest"){
-          # T-test model
-          fileName <- paste(start, "_ttestEZBHDDM", sep="")
-  }else{  
-          # Invalid model type
-          stop("Invalid modelType")  
-  }}}
-  
-  # Filename should specify the parameter generation method
-  if(fromPrior){
-        # Parameters generated from prior distributions
-        fileName <- paste(fileName,"_genPrior.RData", sep="")
-  }else{
-        # Parameters generated from uniform distributions
-        fileName <- paste(fileName,"_genUnif.RData", sep="")
-  }
-  
-  return(fileName)
+nameOutput <- function(nTrials, nParticipants, nDatasets, modelType, fromPrior, output.folder = NA) {
+    # Set default output folder using here() if not specified
+    if(is.na(output.folder)) {
+        output.folder <- here::here("output", "RData-results")
+    }
+    
+    # Ensure output folder exists
+    if(!dir.exists(output.folder)){
+        dir.create(output.folder, recursive = TRUE)
+    }
+    
+    # Create a compact base filename
+    start <- paste("sim_P", nParticipants, "T", nTrials, "D", nDatasets, sep="")
+    
+    # Add a short code for model type
+    modelCode <- switch(modelType,
+                        "hierarchical" = "Hier",
+                        "metaregression" = "Meta",
+                        "ttest" = "Ttst",
+                        "X")  # Default if unknown
+    
+    # Add model code and prior indicator
+    filename <- paste0(start, "_", modelCode, "_", ifelse(fromPrior, "fromPrior", "fromUnif"), ".RData")
+    
+    # Combine folder and filename, ensuring proper path separator
+    if(substr(output.folder, nchar(output.folder), nchar(output.folder)) != "/" && 
+       substr(output.folder, nchar(output.folder), nchar(output.folder)) != "\\") {
+        output.folder <- paste0(output.folder, "/")
+    }
+    outputFile <- paste0(output.folder, filename)    
+    
+    return(outputFile)
 }
