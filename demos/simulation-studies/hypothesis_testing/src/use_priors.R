83ff7e67a8dede073e798ead77db7d6573401c91
Adriana Chavez <adrifelcha@gmail.com>
2025-03-18 18:48:14 -0700

    A function to run a simple simulation study for a given set of simulation settings

--- demos/simulation-studies/hypothesis_testing/src/use_priors.R
+++
@@ -1,8 +0,0 @@
-priors <- data.frame("bound_mean_mean" = 1.5,  "bound_mean_sdev" = 0.2, 
-                     "drift_mean_mean" = 0.00,  "drift_mean_sdev" = 0.50, 
-                     "nondt_mean_mean" = 0.30,  "nondt_mean_sdev" = 0.06, 
-                     "bound_sdev_lower" = 0.10,  "bound_sdev_upper" = 0.40,   
-                     "drift_sdev_lower" = 0.20,  "drift_sdev_upper" = 0.40,
-                     "nondt_sdev_lower" = 0.025,  "nondt_sdev_upper" = 0.25,
-                     "betaweight_mean" = 0.00, "betaweight_sdev" = 1.00)
-
