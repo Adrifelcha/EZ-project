9048d246558c3cf39795b265a3a1bf51aca22bb5
Adriana Chavez <adrifelcha@gmail.com>
2025-04-20 13:33:46 -0700

    Folder has been moved to demos/simulation-studies

--- simulations/hypothesis_testing/scripts/show_design.R
+++
@@ -1,7 +0,0 @@
-show_design = function(settings){
-      cat("== Within-subject t-test design on the drift rate\n")
-      cat("Number of Participants:  ", settings$nPart,"\n")
-      cat("Trials Per Condition:    ", settings$nTrialsPerCondition,"\n")
-      cat("Number of Conditions:    ", 2,"\n")
-      cat('Iterations to run:       ', settings$nDatasets,"\n")
-}
