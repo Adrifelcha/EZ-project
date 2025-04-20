75d531f18cd3ec2ddecd9a8e237b8b4f191fbafa
Adriana Chavez <adrifelcha@gmail.com>
2025-04-19 19:34:34 -0700

    Update runParallel script

--- demos/simulation-studies/generative_priors/runParallelSeeds.R
+++ demos/simulation-studies/generative_priors/runParallelSeeds.R
@@ -2,38 +2,60 @@
 # LOAD FUNCTIONS/PACKAGES
 ##########################################################
 ######## Load required R packages for parallel processing
-for(archive in dir("../../code/functions/")){    source(paste("../../code/functions/",archive,sep=""))     }
-for(archive in dir("./functions/")){    source(paste("./functions/",archive,sep=""))     }
-source("../../code/scripts/HDDM_setup.R")
-source("../../code/scripts/HDDM_runJAGS.R")
+library(here)
 library(foreach)
 library(doParallel)
+
+########| Load required R scripts
+skip_scripts <- c("README.md")
+cat("Sourcing scripts...\n")
+for(archive in dir(here("src"))){   
+    if(archive %in% skip_scripts){
+        next
+    }else{                
+        cat(paste("Sourcing:", archive, "\n"))
+        source(here("src", archive))        
+    }    
+}
 
 ##########################################################
 # SIMULATION SETTINGS
 ##########################################################
-#######| Create fixed variables to be used during the simulations
-jagsParameters <- c("bound_mean", "drift_mean", "nondt_mean", "bound", "nondt",
-                    "drift_sdev", "nondt_sdev", "bound_sdev", "drift", "betaweight")
-modelFile <- matrix(c(rep(c("./EZHBDDM_BetaDrift.bug","./EZHBDDM_BetaNondt.bug","./EZHBDDM_BetaBound.bug"),2)), 
-                    byrow = TRUE, ncol = 3)
-########| Create a "settings" object that specifies all relevant aspects of the simulation study
-#################| Fixed variables
-settings <- list("fromPrior" = TRUE,  # This logical variable should be defined before running this script
-                 "output.folder" = "./samples/", # Before running this script, indicate where to store samples
-                 "participant_levels" = c(20,40,80,160,320), 
+# Create output directory if it doesn't exist
+output_dir <- here("demos", "simulation-studies", "generative_priors", "samples")
+dir.create(output_dir, recursive = TRUE, showWarnings = FALSE)
+
+settings <- list("fromPrior" = TRUE,  
+                 "output.folder" = file.path(output_dir, "/"),  # Ensure trailing slash
+                 "participant_levels" = c(20,40,80,160,320),
                  "trial_levels" = c(20,40,80,160,320),
                  "nDatasets" = 1000,
                  "criterion_levels" = c("drift", "nondt", "bound"),
                  "design_levels" = c("ttest","metaregression"),
                  "n.chains" = 3)
+
+
+
+# Create a "settings" object that specifies all relevant aspects of the simulation study
+#################| Fixed variables
+
 settings <- c(settings,
               list("nCells" = prod(length(settings$participant_levels),length(settings$trial_levels))*(3*2)))
+
 #################| Specific JAGS objects
+jagsParameters <- c("bound_mean", "drift_mean", "nondt_mean", "bound", "nondt",
+                    "drift_sdev", "nondt_sdev", "bound_sdev", "drift", "betaweight")
+
 jagsInits <- list()
     for(i in settings$participant_levels){
-      jagsInits <- c(jagsInits, list(default_inits(settings$n.chains, i)))
+      jagsInits <- c(jagsInits, list(JAGS_inits(settings$n.chains, i, custom_sd = 0.1)))
     }
+
+# Define the model file names depending on the criterion parameter
+modelFile <- matrix(c(rep(c("./EZHBDDM_BetaDrift.bug",
+                            "./EZHBDDM_BetaNondt.bug",
+                            "./EZHBDDM_BetaBound.bug"),2)), 
+                    byrow = TRUE, ncol = 3)
 settings <- c(settings, list("jagsParameters" = list(jagsParameters, jagsParameters),
                              "modelFile" = modelFile,
                              "priors" = list(default_priors(Show=FALSE, "ttest"), default_priors(Show=FALSE, "metaregression")),
@@ -47,9 +69,26 @@ names(settings$jagsInits) <- settings$participant_levels
 colnames(settings$modelFile) <- settings$criterion_levels
 rownames(settings$modelFile) <- settings$design_levels
 ########| Write JAGS models
+
+
+custom_truncation_list <- list(
+                "bound_mean" = c(0.1, 3.0),
+                "nondt_mean" = c(0.05, ""),
+                "drift_mean" = c(-3, 3),
+                "bound_sdev" = c(0.01, ""),
+                "nondt_sdev" = c(0.01, ""),
+                "drift_sdev" = c(0.01, ""),
+                "drift" = c(-3, 3),
+                "bound" = c(0.1, 3.0),
+                "nondt" = c(0.05, ""),
+                "betaweight" = c(-3, 3)
+        )
+
 for(model in settings$design_levels){
     for(crit in settings$criterion_levels){
-        write_JAGSmodel(settings$priors[[2]], model, crit, settings$modelFile[model,crit])
+        write_JAGSmodel(priors = settings$priors[[2]], modelType = model, 
+                        criterion =crit, modelFile = settings$modelFile[model,crit],
+                        custom_truncation_list = custom_truncation_list)
     }
 }
 
