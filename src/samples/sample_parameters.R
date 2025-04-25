af6c02bceeeb967353f4e7554e22e5b4bcd70c08
Adriana Chavez <adrifelcha@gmail.com>
2025-04-24 18:54:24 -0700

    Fix: Partition the function, separating the main function into separate auxiliary functions

--- src/samples/sample_parameters.R
+++ src/samples/sample_parameters.R
@@ -223,3 +223,27 @@ sample_bound <- function(nPart, bound_mean, bound_sdev, X, criterion = NA) {
     }
 return(bound)
 }
+
+
+##############################################
+# Print parameter values to console
+##############################################
+show_parameters <- function(parameter_set){  
+      cat("===== EZBHDDM True Parameters: ==============\n")
+      
+      # Print drift rate hierarchical parameters
+      cat("Drift Mean:   ", parameter_set$drift_mean,"\n")
+      cat("Drift SD:     ", parameter_set$drift_sdev,"\n")
+      # Print decision boundary hierarchical parameters
+      cat("Bound Mean:   ", parameter_set$bound_mean,"\n")
+      cat("Bound SD:     ", parameter_set$bound_sdev,"\n")
+      # Print non-decision time hierarchical parameters
+      cat("Non-decision Time Mean:", parameter_set$nondt_mean,"\n")
+      cat("Non-decision Time SD:  ", parameter_set$nondt_sdev,"\n")
+      # Print betaweight parameter if it exists
+      if(!is.null(parameter_set$betaweight)){
+        cat("Betaweight:   ", parameter_set$betaweight,"\n")
+      }
+        
+      cat("=============================================\n")
+}
