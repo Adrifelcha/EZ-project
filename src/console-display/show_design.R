64ebac81b91b3afa82839ad0acaba5cabe0619bf
Adrifelcha <adrifelcha@gmail.com>
2025-05-19 13:47:49 -0700

    Fix: The function now works with local settings too, this helps debugging

--- src/console-display/show_design.R
+++ src/console-display/show_design.R
@@ -1,7 +1,7 @@
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~!
 # This function displays the simulation design parameters to the console
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~!
-show_design = function(settings, withinSubjectDesign = FALSE){
+show_design = function(local_settings, withinSubjectDesign = FALSE){
       
       # Print header
       cat("========== EZBHDDM Design Parameters: ==========\n")
@@ -13,28 +13,30 @@ show_design = function(settings, withinSubjectDesign = FALSE){
       } else {           
       # Case 2: Between-subjects designs
             # modelType is either "hierarchical", "metaregression", or "ttest"
-            cat("Model type:              ", settings$modelType,"\n")
+            cat("Model type:              ", local_settings$modelType,"\n")
             
             # For "metaregression" and "ttest" models, 
             # indicate which parameter is being tested for effects
-            if(!(settings$modelType=="hierarchical"|is.na(settings$modelType))){
-                  cat("Testing for an effect on:", settings$criterion,"\n") 
+            if(!(local_settings$modelType=="hierarchical"|is.na(local_settings$modelType))){
+                  cat("Testing for an effect on:", local_settings$criterion,"\n") 
             }
       }
             
       # Display number of participants
-      cat("Number of Participants:  ", settings$nPart,"\n")
+      cat("Number of Participants:  ", local_settings$nPart,"\n")
       
       # Display trial information
-      if(withinSubjectDesign || !is.null(settings$nTrialsPerCondition)){
+      if(withinSubjectDesign || !is.null(local_settings$nTrialsPerCondition)){
             # Within-subject design
-            cat("Trials Per Condition:    ", settings$nTrialsPerCondition,"\n")
+            cat("Trials Per Condition:    ", local_settings$nTrialsPerCondition,"\n")
             cat("Number of Conditions:    ", 2,"\n")
       } else {
             # Between-subjects design
-            cat("Trials Per Person:       ", settings$nTrials,"\n")
+            cat("Trials Per Person:       ", local_settings$nTrials,"\n")
       }
       
       # Display number of simulation iterations
-      cat('Iterations to run:       ', settings$nDatasets,"\n")
+      if(!is.null(local_settings$nDatasets)){
+            cat('Iterations to run:       ', local_settings$nDatasets,"\n")
+      }
 }
