2219cc6add6ffb22e4fdff65a516584de52acdf8
Adrifelcha <adrifelcha@gmail.com>
2025-05-18 22:36:40 -0700

    Remove tmp file

--- tmp.R
+++
@@ -1,6 +0,0 @@
-thisMn <- which(outB[[k]][,"p"]==p&outB[[k]][,"t"]==t&outB[[k]][,"c"]=="nondt"&outB[[k]][,"d"]=="metaregression")
-
-outB[[k]][thisMn,"p"]
-outB[[k]][thisMn,"mean.estimates"]$mean.estimates$nondt
-
-outB[[k]][thisMn,]
